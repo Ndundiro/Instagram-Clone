98f0a722df0b38128b86db001dcb2bc630bcca24
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-11 11:18:43 +0300

    Initialise the project Insta.

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/Moringa-Core/Django.week2/Insta-IP/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
