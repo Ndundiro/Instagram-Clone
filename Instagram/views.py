ef67723d911ed4ad55338043cae9bc73015b062f
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-12 22:49:33 +0300

    Updates the views for home and welcome page,and my base.html template.

--- Instagram/views.py
+++ Instagram/views.py
@@ -3,8 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author':'jghjg',
+        'image':'image',
+        'caption':'caption',
+        'comment':'comment'
+    }
+]
+
+
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'posts':posts})
 
 
 def about(request):
