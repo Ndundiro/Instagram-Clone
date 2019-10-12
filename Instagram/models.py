ac30d4542548e5360e7c3130c86bd0d0adafabcd
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-13 01:03:55 +0300

    Update the Post model class,description field changed to caption.

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    decription = models.TextField()
+    caption = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.caption
