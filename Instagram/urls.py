3b97b206fec77c9e166840be7767558e8cacfeba
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-14 12:42:49 +0300

    Adds the url path to post update view and new post view

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PosListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     # path('', views.home, name='insta-home'),
     path('about/', views.about, name="insta-about"),
-    path('', PosListView.as_view(), name='insta-home'),
+    path('', PostListView.as_view(), name='insta-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
 
 ]
