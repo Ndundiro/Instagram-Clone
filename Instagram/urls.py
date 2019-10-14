76cb8cc34dfc3fc872864bede786e4454c644a95
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-14 09:32:09 +0300

    Change the view function to class based views.

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PosListView
 
 urlpatterns = [
-    path('', views.home, name='insta-home'),
+    # path('', views.home, name='insta-home'),
     path('about/', views.about, name="insta-about"),
-
+    path('', PosListView.as_view(), name='insta-home'),
 
 ]
--- Instagram/views.py
+++ Instagram/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post
+from django.views.generic import  ListView
 
 # Create your views here.
 
-def home(request):
-    context ={
-    'posts':Post.objects.all()
-    }
-    return render(request, 'index.html', context )
+# def home(request):
+#     context ={
+#     'posts':Post.objects.all()
+#     }
+#     return render(request, 'index.html', context )
+
+
+class PosListView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
 
 
 def about(request):
