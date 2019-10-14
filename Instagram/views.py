29818d7185a655b8395a10950cfead63472a1447
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-14 12:41:49 +0300

    adds a postcreateview and post update view class based view

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import  ListView,DetailView, CreateView
+from django.views.generic import  ListView,DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -11,16 +12,25 @@ from django.views.generic import  ListView,DetailView, CreateView
 #     return render(request, 'index.html', context )
 
 
-class PosListView(ListView):
+class PostListView(ListView):
     model = Post
     template_name = 'index.html'  #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['caption']
+    fields = ['caption', 'image']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['caption', 'image']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
